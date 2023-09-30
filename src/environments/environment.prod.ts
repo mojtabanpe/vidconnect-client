496220b6aaab038cff6ea5ab5fdc0517828f171e
mojtabanpe <memnpe@gmail.com>
2023-10-01 00:06:30 +0330

    change ws url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // ws_url: 'wss://vidconnect-xh8yur1m.livekit.cloud',
-  ws_url: 'ws://localhost:7880',
+  ws_url: 'wss://vidconnect-xh8yur1m.livekit.cloud',
+  // ws_url: 'ws://localhost:7880',
   server_url: 'http://localhost:3000/api/v1/',
 };
