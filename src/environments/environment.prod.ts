6c4d6c6a949664eea60aed80f80199c7e7731d77
mojtabanpe <43112914+mojtabanpe@users.noreply.github.com>
2023-10-01 22:41:46 +0330

    9 mehr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ws_url: 'wss://vidconnect-xh8yur1m.livekit.cloud',
   // ws_url: 'ws://localhost:7880',
-  server_url: 'http://vidconnect.online/api/',
+  server_url: 'https://vidconnect.online/api/',
 };
